703d7c6563b8d9402d2527624ef0f0e20ad77d7f
robert_nicolescu <robert.nicolescu1992@gmail.com>
2023-10-16 21:18:35 +0300

    .

--- WEB-PAGE_TESTING/SMOKE_TESTING/1_ACCESS_WEB_PAGE/features/steps/smoke_testing.py
+++ WEB-PAGE_TESTING/SMOKE_TESTING/1_ACCESS_WEB_PAGE/features/steps/smoke_testing.py
@@ -19,7 +19,7 @@ def step_maximize_web_browser_window(context):
 
 @given('Make request to "{url}"')
 def step_make_request(context, url):
-    context.response = requests.test1
+    context.response = requests.get(url)
 
 @when('Request is 200')
 def step_request_is_200(context):
